01b7c4c774ae3fd16f29c1cfeb07a31188153a97
harshal0151 <patilharshal0151@gmail.com>
2024-05-24 11:11:06 +0530

    React day-1

---
+++ Introduction to ReactJs/Day2- ClassWork/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+      <header>
+      <h1>Learn web development</h1>
+      </header>
+
+      <div>
+        <p>Welcome to the MDN learning area. This set of articles aims to provide complete beginners to web development with fundamental skills for coding websites.</p>
+      </div>
+
+      <div>
+        <p> The aim is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
+      </div>
+
+      <div>
+        <p>If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.</p>
+      </div>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ Introduction to ReactJs/Day2-HomeWork/src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section >
+        <header><h1>Topics covered</h1>
+        <p>The following is a list of all the topics we cover in the MDN learning area.</p>
+        </header>
+
+
+        <div>
+          <a href="#">Getting started with the web</a>
+          <p>Provides a practical introduction to web development for complete beginners.</p>
+        </div>
+
+
+        <div>
+          <a href="#">HTML-Structuring the web</a>
+          <p>HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.</p>
+        </div>
+
+        <div>
+          <a href="#">CSS--Styling the web</a>
+          <p>CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS. </p>
+        </div>
+       
+
+
+
+      </section>
+    </>
+  )
+}
+
+export default App

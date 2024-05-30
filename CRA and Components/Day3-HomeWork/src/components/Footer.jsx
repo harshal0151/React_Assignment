02abd259a4cc46c8033e86cf9bb8f3aeaa27986f
harshal0151 <patilharshal0151@gmail.com>
2024-05-30 19:49:22 +0530

    Family Willness

---
+++ CRA and Components/Day3-HomeWork/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer(){
+  return(
+    <>
+    <footer>
+        <h1>Family Willness Massage Therapy</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Phone - 9284117732</p>
+        <p className="copyright">Copyright2034  @Harshal Patil All Rights Reserved</p>
+        <p>Powered By : Harshal.com</p>
+    </footer>
+    </>
+  )
+}
+export default Footer;

1250308ba6f01bc47585f40c7d2b4d188aebfdc1
harshal0151 <patilharshal0151@gmail.com>
2024-06-19 10:26:56 +0530

    text transletor

--- Api/day9-ClassWork/src/App.jsx
+++ Api/day9-ClassWork/src/App.jsx
@@ -1,40 +1,51 @@
-
-import { useEffect } from 'react';
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
- 
+  const [exercises, setExercises] = useState([]);
 
-async  function data(){
-    const url = 'https://work-out-api1.p.rapidapi.com/search?Muscles=biceps';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '280bfe97c4msh84b84ada3f78524p1e7593jsn6cd59edb075b',
-		'x-rapidapi-host': 'work-out-api1.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response;
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-  }
+  useEffect(() => {
+    async function fetchData() {
+      const options = {
+        method: 'GET',
+        url: 'https://work-out-api1.p.rapidapi.com/search',
+        params: { Muscles: 'biceps' },
+        headers: {
+          'x-rapidapi-key': '280bfe97c4msh84b84ada3f78524p1e7593jsn6cd59edb075b',
+          'x-rapidapi-host': 'work-out-api1.p.rapidapi.com'
+        }
+      };
 
-useEffect(()=>{
-  data()
+      try {
+        const response = await axios.request(options);
+        const data = response.data;
+        console.log(data);
+        setExercises(data);
+       
+      } catch (error) {
+        console.error(error);
 
-  },[]) 
+       
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
-    <>
-
-    <h1>Hello</h1>
-    </>
-  )
+    <div className="App">
+     
+       {  exercises.map((exercise, index) => (
+          <div key={index} className="exercise">
+            <h3>{exercise.Muscles}</h3>
+           <video src= {exercise.video}></video>
+          </div>
+        ))
+      }
+    </div>
+    
+  );
 }
 
-export default App
+export default App;
